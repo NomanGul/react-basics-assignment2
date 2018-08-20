e30daffbf3e17c840e6259666c20ebe8286787a5
Noman Gul <nomangul2001@gmail.com>
2018-08-20 23:55:54 +0500

    add navbar & css

--- src/App.js
+++ src/App.js
@@ -6,17 +6,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// NAVBAR COMPONENT
+class Navbar extends Component {
+  render() {
+    return (
+        <div className="navbar-fixed">
+          <nav>
+            <div className="nav-wrapper grey darken-3 z-depth-4">
+              <a className="brand-logo center"><img src={logo} className="App-logo center-align" alt="logo" /></a>
+            </div>
+          </nav>
+        </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar />
+
+        <div className = "container">
+
+        </div>
       </div>
     );
   }
