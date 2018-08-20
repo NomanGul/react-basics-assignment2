503d54f3a90bcf95da7f5b0c0d83d3dc45bd0c5c
Noman Gul <nomangul2001@gmail.com>
2018-08-21 01:52:42 +0500

    add q1 & author intro component and css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import myImg from './subHeaderPhoto.jpg';
 
 // NAVBAR COMPONENT
 class Navbar extends Component {
@@ -21,6 +22,50 @@ class Navbar extends Component {
   }
 }
 
+// AUTHOR INTRODUCTION COMPONENT
+class AuthorIntro extends Component {
+  render() {
+    return (
+      <div>
+        <a href="https://www.linkedin.com/in/nomangul2001/" target="_blank" rel="noopener noreferrer">
+          <div className="col s12 m8 offset-m2 l6 offset-l3 author-card">
+            <div className="card-panel blue darken-1 hoverable">
+              <div className="row valign-wrapper">
+                <div className="col s2">
+                  <img src={myImg} alt="Author" className="circle responsive-img" />
+                </div>
+                <div className="col s10">
+                  <span className="white-text"> This React project is developed by Noman Gul. I hope you like this, So dont forget to give your feedback!</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    )
+  }
+}
+
+
+class Q1 extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Hello World',
+      boolean: true
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.text}</h1>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -28,6 +73,8 @@ class App extends Component {
         <Navbar />
 
         <div className = "container">
+          <AuthorIntro />
+          <Q1 />
 
         </div>
       </div>
